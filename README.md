a7dfe9e14df96678f4b0d4166da6036df9f07275
takuyara <takuyara2136@gmail.com>
2020-12-31 15:55:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ToyCCompiler
+ 
---
+++ ToyCGenerator.py
@@ -0,0 +1,4 @@
+# Pionniers du TJ, benissiez-moi par votre Esprits Saints!
+class ToyCGenerator(ToyCVisitor):
+	def __init__(self):
+		super(ToyCVisitor, self).__init__()
